06f504a0db715b91b3db6b7a86d5ee5dce266988
Marc de Groot <marc.degroot@kpn.com>
2019-02-24 08:23:15 +0100

    First commit Heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -9,10 +9,10 @@ import { HeroesComponent } from './heroes/heroes.component';
   declarations: [
     AppComponent,
     HeroesComponent,
-    // FormsModule
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
